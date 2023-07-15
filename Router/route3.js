81d8ba344f431bf552ef7896d2dc6f78c7c2945a
subhendu20 <64896535+subhendu20@users.noreply.github.com>
2023-07-15 20:46:10 +0530

    new

--- Router/route3.js
+++ Router/route3.js
@@ -14,7 +14,7 @@ const cookie = require('cookie-parser')
 
 router.use(cookie())
 router.use(cors({
-          origin: 'http://localhost:6001',
+          origin: 'http://localhost:6001/*',
           methods: ['POST', 'PUT', 'GET','OPTIONS', 'HEAD'],
           credentials: true
 }))
